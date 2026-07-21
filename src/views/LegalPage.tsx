dd737add385ae028a8378f25a49cd80c604ad307
Smarak Das <179333759+SmarakDA5@users.noreply.github.com>
2026-07-21 08:50:51 +0530

    Add useLocation import to LegalPage component

--- src/views/LegalPage.tsx
+++ src/views/LegalPage.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useLocation } from 'react-router-dom';
 
 interface LegalPageData {
   slug: string;
